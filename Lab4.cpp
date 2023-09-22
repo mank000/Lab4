8af3c8c41e9991fddf298e7fcc81d9a6d4f09238
mank000 <kozmin_arti@mail.ru>
2023-09-22 12:32:36 +0700

    fixes

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,36 +1,51 @@
 ﻿#include <iostream>
 #include <clocale>
 using namespace std;
-int solution1() {
+
+/**
+ * 12. Дан набор ненулевых целых чисел; признак его завершения - число 0.
+ * Вывести сумму всех положительных четных чисел из данного набора.
+ * Если требуемые числа в наборе отсутствуют, то вывести 0.
+*/
+void solution1() {
     int UserNumber;
     int SummOfChet = 0;
     while (true) {
         cin >> UserNumber;
-        if ((UserNumber % 2) == 0
-            and (UserNumber != 0)
-            and (UserNumber > 0)) { SummOfChet += UserNumber; }
-        else if (UserNumber == 0) { break; }
+        if (UserNumber == 0) { break; }
+        if ((UserNumber % 2) == 0)    {
+            SummOfChet += UserNumber; 
+        }
     }
-    cout << "\n";
     cout << "Ваша сумма чисел:" << SummOfChet << "\n";
-    return 0;
 }
 
+/**
+ * @brief 12. Дано целое число N (> 0). Найти произведение 1.1 * 1.2 * 1.3 * ... (N сомножителей).
+*/
 int solution2() {
     int UserNumber;
-    float AllProiz = 1;
+    float AllProiz = 1.0;
     float Multipier = 1.0;
     cin >> UserNumber;
-    for (int key = 0; UserNumber != key; key++)
-    {
+    if (UserNumber <= 0) {
+        cerr << "Число должно быть > 0";
+        return 1;
+    }
+    for (int i = 0; i < UserNumber; i++) {
         Multipier += 0.1;
         AllProiz *= Multipier;
     }
     cout << AllProiz << "\n";
     return 0;
 }
+
+/**
+ * @brief Лр 4. Циклический вычислительный процесс
+ * по заданиям
+*/
 int main()
 {   
     setlocale(LC_ALL, "ru");
-    cout << solution2();
+    return solution2();
 }
